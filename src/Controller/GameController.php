f84f117370b77b5503949ee684b3802faf397b92
Stéphane Sauvaget <s.sauvaget41000@gmail.com>
2021-04-16 12:18:42 +0200

    Correct pages from old PR, change branch name

---
+++ src/Controller/GameController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+class GameController extends AbstractController
+{
+    /**
+     * Display home page
+     *
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+
+    public function game()
+    {
+        return $this->twig->render('Game/game.html.twig');
+    }
+}
